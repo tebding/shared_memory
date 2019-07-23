15bba8fa291dffbfe7e47cb591163a5bfe8de46d
tebding <tyler.ebding@gmail.com>
2019-07-23 00:46:50 -0700

    renamed main file. made debugging progress

--- shared_memory.c
+++ shared_memory.c
@@ -2,9 +2,9 @@
 
 #include <string.h>
 #include <unistd.h> //probably not necessary?
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/mman.h> //enables mmap()
 #include <fcntl.h> //for O_* constants
 #include <sys/stat.h> //for mode constants
@@ -33,8 +33,9 @@ RETURN: 1 for error, 0 for success
 */
 int update_shm(FILE *fp, int fd) {
     item pairs[SHM_SIZE]; //array of entries: CAN SHORTEN IF LONGER THAN NEEDED
-    char* buffer, tok; //for reading from file
-    char delim = ' ';
+    char* buffer; //for reading from file
+    char* tok;
+    const char delim[2] = " ";
     int i, index, k, j = 0; //for handling loops + table
 
     //read input file into buffer, then tokenize buffer. repeat until EOF
@@ -42,9 +43,9 @@ int update_shm(FILE *fp, int fd) {
         tok = strtok(buffer, delim); //tokenize the line
         while (tok != NULL) {
             //iterates through tokens for each line.
-            pairs[i]->key = atoi(tok); //first item is key
+            pairs[i].key = atoi(tok); //first item is key
             tok = strtok(NULL, delim); //tokenize to 2nd item
-            pairs[i]->val = tok; //second item is value
+            strcpy(pairs[i].val, tok); //second item is value
             i++;
         }
         //repeats this loop for each line from the input file.
@@ -61,10 +62,10 @@ int update_shm(FILE *fp, int fd) {
     //hash keys, and copy values into shm indices
     while (j < i) {
         //find which index to write to for current index of 'pairs'
-        index = hash(pairs[j]->key);
+        index = hash(pairs[j].key);
         //linearly probe to resolve collisions
         //will overwrite stored values with the same key
-        while ((table[index]->key != NULL || table[index]->key != pairs[j]->key)
+        while ((table[index].key != NULL || table[index].key != pairs[j].key)
                 && (k < SHM_SIZE)) {
             index++;
             k++; //tracks number of increments to prevent infinite loop
@@ -74,11 +75,11 @@ int update_shm(FILE *fp, int fd) {
             return 1;
         }
         //by including the key in the table, can easily check collisions.
-        table[index]->key = pairs[j]->key; //redundant if overwriting...
+        table[index].key = pairs[j].key; //redundant if overwriting...
 
         //note: I'd use strncpy() but I can't identify any case where it's needed
         //and the required extra call to strlen() or sizeof() would be slower.
-        strcpy(table[index]->val, pairs[j]->val);
+        strcpy(table[index].val, pairs[j].val);
 
         //to handle next key/val pair write
         j++;
@@ -105,8 +106,9 @@ RETURN: 1 for error, 0 for success
 int delete_shm(FILE *fp, int fd) {
     int keys[SHM_SIZE]; //int array is cheaper than item array + still suffices
     int i, index, k, j = 0;
-    char* buffer, tok; //for file reading
-    char delim = ' ';
+    char* buffer; //for file reading
+    char* tok;
+    const char delim[2] = " ";
 
     //read the input file into buffer, then tokenize the input. repeat until EOF
     while (fgets(buffer, BUFF_SIZE, fp) != NULL) { //read input line -> buffer
@@ -134,22 +136,22 @@ int delete_shm(FILE *fp, int fd) {
     while (j < i) {
         index = hash(keys[j]);
         //find items that were collisions
-        while ((table[index]->key != keys[j]) && (k < SHM_SIZE) {
-            if (table[index]->key == NULL) {
+        while ((table[index].key != keys[j]) && (k < SHM_SIZE)) {
+            if (table[index].key == NULL) {
                 break; //error handled in next 'if' statement
             }
             index++;
             k++;
         }
-        if (k >= SHM_SIZE || table[index]->key == NULL) {
-            frpintf(stderr, "ERROR: key \"%d\" not found.", keys[j]);
+        if (k >= SHM_SIZE || table[index].key == NULL) {
+            fprintf(stderr, "ERROR: key \"%d\" not found.", keys[j]);
         }
 
         //actual deletion process
         for (k = 0; k < BUFF_SIZE; k++) { //k can be re-used to save time
             //clears every index of the string for security.
-            if (table[index]->val[k] != '\0') {
-                table[index]->val[k] = '\0';
+            if (table[index].val[k] != '\0') {
+                table[index].val[k] = '\0';
                 k++;
             }
             else { //at end of string. skip the trailing emptiness to save cycles
@@ -157,10 +159,10 @@ int delete_shm(FILE *fp, int fd) {
             }
             /*
             IF SPEED > SECURITY, use this INSTEAD OF above 'if-else' statement!
-            table[index]->val = '\0';
+            table[index].val = '\0';
             */
         }
-        table[index]->key = NULL; //clears the key
+        table[index].key = NULL; //clears the key
 
         //handle the next key in next loop iteration
         j++;
@@ -185,11 +187,12 @@ PARAMS:
     fd: file descriptor to reference "shm" file, as determined in main()
 RETURN: 1 for failure, 0 for success
 */
-void print_shm(FILE *fp) {
+int print_shm(FILE *fp, int fd) {
     int keys[SHM_SIZE]; //only keys are needed to ID index to print
     int i, index, k, j = 0; //for looping
-    char* buffer, tok; //for reading input file
-    char delim = ' ';
+    char* buffer; //for reading input file
+    char* tok;
+    const char delim[2] = " ";
 
     //read the input file into buffer, then tokenize the input. repeat until EOF
     while (fgets(buffer, BUFF_SIZE, fp) != NULL) { //read input line -> buffer
@@ -216,20 +219,20 @@ void print_shm(FILE *fp) {
     while (j < i) {
         index = hash(keys[j]);
         //find items that were collisions
-        while ((table[index]->key != keys[j]) && (k < SHM_SIZE)) {
-            if (table[index]->key == NULL) {
+        while ((table[index].key != keys[j]) && (k < SHM_SIZE)) {
+            if (table[index].key == NULL) {
                 break;
             }
             index++;
             k++; //track iterations to prevent infinite looping
         }
         //if key doesn't exist
-        if (k >= SHM_SIZE || table[index]->key == NULL) {
+        if (k >= SHM_SIZE || table[index].key == NULL) {
             fprintf(stderr, "ERROR: key \"%d\" not found.", keys[j]);
         }
 
         //executes the printing for the current key
-        printf("key %d has value: %s", table[index]->key, table[index]->val)
+        printf("key %d has value: %s", table[index].key, table[index].val);
 
         //handle the next key in next loop iteration
         j++;
@@ -239,21 +242,21 @@ void print_shm(FILE *fp) {
 
 
 int main(int argc, char const *argv[]) {
-    int i;
     /*
         I opted against combining the shm_open() calls in order to
         eliminate the otherwise-inevitable calls to ftruncate() that
         are entirely unnecessary except for when O_CREAT is invoked.
     */
-    int fd = shm_open("shm", O_RDWR);
+    int fd = shm_open("shm", O_RDWR, 0666);
     if (fd == -1) { //memory space does not yet exist
         fd = shm_open("shm", O_CREAT, 0666);
         if (fd == -1) { //failed to create memory space
             perror("failed to open/create:");
             return 1;
         }
+        int size = SHM_SIZE*sizeof(item);
         //set the space to the desired size
-        if (ftruncate(fd, SHM_SIZE*sizeof(item)) == -1) {
+        if (ftruncate(fd, size) == -1) {
             perror("failed to set shared memory space size:");
             return 1;
         }
@@ -279,7 +282,8 @@ int main(int argc, char const *argv[]) {
             return 1;
         }
         else {
-           printf("successfully updated shared memory with data from %s", argv[1]);
+           printf("successfully updated shared memory with data from %s",
+                    argv[1]);
         }
     }
     else if (argv[2] == 2) { //delete
@@ -299,7 +303,7 @@ int main(int argc, char const *argv[]) {
         }
     }
     else {
-        fprintf(stderr, "invalid operation: %d", argv[2]);
+        fprintf(stderr, "invalid operation: %s", argv[2]);
         return 1;
     }
 
